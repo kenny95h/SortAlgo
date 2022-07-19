e96e0270ccf646e4710203796b040c1a14e3b650
Kenny <kharvey@spartaglobal.com>
2022-07-19 16:06:27 +0100

    Base program files created. BubbleSort class and BubbleSortTests.

---
+++ Sort/SortApp/BubbleSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortApp
+{
+    public class BubbleSort
+    {
+        public int[] BubbleSorting(int[] array)
+        {
+            return array;
+        }
+    }
+}
---
+++ Sort/SortTests/BubbleSortTests.cs
@@ -0,0 +1,14 @@
+using SortApp;
+
+namespace SortTests
+{
+    public class BubbleSortTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
