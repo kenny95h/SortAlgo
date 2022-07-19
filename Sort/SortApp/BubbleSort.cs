09804ecc50b95ea86e35690569e34c8e337b25df
Mafundii <makshadys1@gmail.com>
2022-07-19 16:48:24 +0100

    Update BubbleSort.cs
    Method commit

--- Sort/SortApp/BubbleSort.cs
+++ Sort/SortApp/BubbleSort.cs
@@ -8,8 +8,21 @@ namespace SortApp
 {
     public class BubbleSort
     {
-        public static int[] BubbleSorting(int[] array)
+        public int[] BubbleSorting(int[] array)
         {
+            int num;
+            for (int x = 0; x <= array.Length; x++)
+            {
+                for (int y = 0; y <= array.Length; y++)
+                {
+                    if (array[y] > array[y + 1])
+                    {
+                        num = array[y + 1];
+                        array[y + 1] = array[y];
+                        array[y] = num;
+                    }
+                }
+            }
             return array;
         }
     }
